a0ebf4652dfea1cee6f824625ea96cc953b4465c
Yathannsh <yathannsh@gmail.com>
2015-12-28 03:45:37 +0530

    Added the webpage

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>NAME!</title>
+    
+    
+   <link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+    <link rel='stylesheet prefetch' href='http://daneden.github.io/animate.css/animate.min.css'>
+<link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,400italic,700italic,700'>
+
+        <link rel="stylesheet" href="css/style.css">
+
+    
+    
+    
+  </head>
+
+  <body>
+
+    <div class="form animated flipInX">
+    <div align="center">
+  <h2>Enter your name</h2></div>
+  <form action="name.php" method="post">
+    <input name="q" placeholder="Name" type="text"></input>
+<button type="submit">Submit</button>
+</form>
+</div>
+   <footer class="foot">
+    Made for <a href= "http://fossasia.org" target="_blank">FOSSASIA</a>
+   </footer>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script> 
+  </body>
+</html>
---
+++ name.php
@@ -0,0 +1,36 @@
+
+
+<!DOCTYPE html>
+<html >
+  <head>
+    <meta charset="UTF-8">
+    <title>NAME!</title>
+    
+    
+       <link href='https://fonts.googleapis.com/css?family=Orbitron' rel='stylesheet' type='text/css'>
+    <link rel='stylesheet prefetch' href='http://daneden.github.io/animate.css/animate.min.css'>
+<link rel='stylesheet prefetch' href='http://fonts.googleapis.com/css?family=Roboto:400,100,400italic,700italic,700'>
+
+        <link rel="stylesheet" href="css/style.css">
+
+    
+    
+  </head>
+
+  <body>
+
+    <div class="form animated flipInX">
+    <div align="center">
+  <h2>Hello, <?php print($_POST["q"]); ?><br>
+   Nice to meet you!</h2></div></div>
+   <footer class="foot">
+    Made for <a href= "http://fossasia.org" target="_blank">FOSSASIA</a>
+   </footer>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    
+    
+    
+    
+  </body>
+</html>
